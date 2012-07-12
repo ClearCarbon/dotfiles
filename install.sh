6bf8f1c0acb815e8c618aa47b93b9baf478e6e0c
Jon Doveston <jon@llamadigital.net>
2012-07-12 12:01:25 +0100

    removed ruby

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for file in vimrc tmux.conf bash_profile bashrc ackrc muttrc vim bash mutt
+for file in vimrc tmux.conf bash_profile bashrc ackrc muttrc vim bash mutt gitignore
 do
   if [ -a $HOME/.${file} ]
   then echo ".$file found, doing nothing"
